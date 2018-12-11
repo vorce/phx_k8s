eac4b1edd27fb8d9dc003efd3d7fe59397f4e3b8
Joel Carlbark <joel.carlbark@meltwater.com>
2018-12-11 14:53:52 +0100

    Use Ecto.Migrator instead of shelling out to mix to run migrations

--- lib/migrator_web/migrate_worker.ex
+++ lib/migrator_web/migrate_worker.ex
@@ -3,6 +3,8 @@ defmodule MigratorWeb.MigrateWorker do
 
   require Logger
 
+  @migrations_dir "#{:code.priv_dir(:migrator)}/repo/migrations"
+
   def start_link() do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
@@ -18,10 +20,10 @@ defmodule MigratorWeb.MigrateWorker do
 
     :timer.sleep(:timer.seconds(10))
 
-    {result, exit_code} = System.cmd("mix", ["ecto.migrate"])
-    Logger.info("Result of migrations (#{exit_code}): #{result}")
+    result = Ecto.Migrator.run(Migrator.Repo, @migrations_dir, :up, all: true)
+    Logger.info("Result of migrations: #{inspect(result)}")
 
-    if exit_code == 0 do
+    if is_list(result) do
       Logger.info("Migrations successful, starting all other endpoints")
       {:ok, _pid} = result = MigratorWeb.DynamicEndpoint.start_endpoint()
       {:stop, :normal, result}
